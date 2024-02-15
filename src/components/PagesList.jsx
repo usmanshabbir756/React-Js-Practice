0f7da86d67567fefcedb2d7b21eccd989b7f814e
usmanshabbir756 <usmanshabbir756@gmail.com>
2024-02-15 18:33:19 +0500

    useState and lift state up

--- src/components/PagesList.jsx
+++ src/components/PagesList.jsx
@@ -1,12 +1,29 @@
+import NewPost from "./NewPost"
 import Page from "./Page"
 import classes from './PagesList.module.css'
+import { useState } from "react";
 
 function PagesList() {
+  const [enteredBody,setEnteredBody]= useState('');
+  const [enteredAuthor,setEnteredAuthor]= useState('');
+
+  function bodyChangeHandler(e){
+      setEnteredBody(e.target.value);
+  }
+
+  function authorChangeHandler(e){
+    setEnteredAuthor(e.target.value);
+}
+
+
   return (
+    <>
+    <NewPost onBodyChange={bodyChangeHandler}  onAuthorChange={authorChangeHandler}/>
     <ul className={classes.posts}>
-        <Page author="Usman" body="React js is awesome!" />
+        <Page author={enteredAuthor} body={enteredBody} />
         <Page author="UMAR" body="jAVA is awesome!" />
     </ul>
+    </>
   )
 }
 
