2bf27a647ed188ab09b8130cbc9641469e78adbc
usmanshabbir756 <usmanshabbir756@gmail.com>
2024-02-16 18:27:52 +0500

    header and post button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import MainHeader from "./components/MainHeaderr";
 import PagesList from "./components/PagesList";
+import { useState } from "react";
 
 function App() {
+  const [modelIsVisible , setModelIsVisible] = useState(false);
+
+  function hideModelHandler(){
+    setModelIsVisible(false)
+  }
+
+  function showModelHandler(){
+    setModelIsVisible(true)
+  }
+
+
   return (
-  <main> 
-    <PagesList />
-  </main>
+    <>
+        <MainHeader onCreatePost={showModelHandler}  />
+        <main> 
+          <PagesList onCloseModel={hideModelHandler} isCreatePostVisible={modelIsVisible}/>
+        </main>
+  </>
   );
 }
 
